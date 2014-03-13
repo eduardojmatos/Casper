afee3961ff129423576e5499ce8bbc0127c948b4
Eduardo Matos <eduardoj.matos@gmail.com>
2014-03-13 18:51:19 -0300

    Merge com js/index do upstream

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,29 +4,39 @@
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+  "use strict";
 
-    $(document).ready(function(){
+  $(document).ready(function(){
 
-        $(".post-content").fitVids();
+    // On the home page, move the blog icon inside the header
+    // for better relative/absolute positioning.
 
-        function casperFullImg() {
-            $("img").each( function() {
-                var contentWidth = $(".post-content").outerWidth(); // Width of the content
-                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+    $(".post-content").fitVids();
 
-                if (imageWidth >= contentWidth) {
-                    $(this).addClass('full-img');
-                } else {
-                    $(this).removeClass('full-img');
-                }
-            });
-        };
+    function casperFullImg() {
+      $("img").each( function() {
+        var contentWidth = $(".post-content").outerWidth(); // Width of the content
+        var imageWidth = $(this)[0].naturalWidth; // Original image resolution
 
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
+        if (imageWidth >= contentWidth) {
+          $(this).addClass('full-img');
+        } else {
+          $(this).removeClass('full-img');
+        }
+      });
+    }
 
-    });
+    casperFullImg();
+    $(window).smartresize(casperFullImg);
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-2909836-3', 'eduardomatos.me');
+    ga('send', 'pageview');
+  });
 
 }(jQuery));
 
@@ -35,25 +45,25 @@
   // debouncing function from John Hann
   // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
   var debounce = function (func, threshold, execAsap) {
-      var timeout;
+    var timeout;
 
-      return function debounced () {
-          var obj = this, args = arguments;
-          function delayed () {
-              if (!execAsap)
-                  func.apply(obj, args);
-              timeout = null;
-          };
+    return function debounced () {
+      var obj = this, args = arguments;
+      function delayed () {
+        if (!execAsap)
+          func.apply(obj, args);
+        timeout = null;
+      }
 
-          if (timeout)
-              clearTimeout(timeout);
-          else if (execAsap)
-              func.apply(obj, args);
+      if (timeout)
+        clearTimeout(timeout);
+      else if (execAsap)
+        func.apply(obj, args);
 
-          timeout = setTimeout(delayed, threshold || 100);
-      };
-  }
-  // smartresize 
+      timeout = setTimeout(delayed, threshold || 100);
+    };
+  };
+  // smartresize
   jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
 })(jQuery,'smartresize');
